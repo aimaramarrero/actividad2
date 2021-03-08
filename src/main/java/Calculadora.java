6c1552fb8c3253547396b68769b07ba8026c194c
Aimara <Aimara@DESKTOP-1NK9TE6>
2021-03-08 20:14:52 +0000

    Paso 1 y 2

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aimara
+ */
+public class Calculadora {
+    private Integer operador1;
+    private Integer operador2;
+    private Integer operacion;
+}
